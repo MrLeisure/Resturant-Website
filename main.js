24f11b41cfd4f9fc457b1ca17131d52c0486a1da
MrLeisure <lseriesgigi@gmail.com>
2022-06-07 23:42:20 +0530

    added gallery.html

--- main.js
+++ main.js
@@ -46,17 +46,84 @@ goto.forEach(e => {
     e.addEventListener('click', (e) => {
         e.preventDefault();
         let [id, speed] = e.target.closest('a').hash.slice(8).split('?');
-        console.log({e,target: e.target, closest: e.target.closest('a'), id});
         let element = document.getElementById(id);
         let dir = Math.sign(element.getBoundingClientRect().top);
-        pageScroll(element, dir, speed || 200);
+        pageScroll(element, dir, speed || 200, window.scrollY );
     })
 })
 
-function pageScroll(element, dir, speed = 200) {
+function pageScroll(element, dir, speed = 200, scrollpos) {
     window.scrollBy(0, dir * speed);
+    if(window.scrollY == scrollpos) return;
     if (element.getBoundingClientRect().top * dir <= 0) return;
     scrolldelay = setTimeout(function() {
-        pageScroll(element, dir, speed);
+        pageScroll(element, dir, speed, window.scrollY);
     },10);
 }
+
+
+//Gallery
+
+var goback = document.querySelector('.gallery_before');
+var goforward = document.querySelector('.gallery_after');
+var galleryList = document.querySelector('.gallery-list')
+
+
+var selected = document.querySelector('.gallery-list > img');
+
+goToGalleryImg(true);
+
+function getSelectedGalleryImg() {
+    if (!galleryList) return null
+
+    var selected = document.querySelector('.gallery-list > .selected');
+    if (selected) return selected;
+
+    selected = galleryList.firstElementChild();
+    selected.classList.add('selected');
+    return selected;
+}
+
+function goToGalleryImg(instant = false) {
+    var rect = selected.getBoundingClientRect();
+    var distance = rect.left + (rect.right - rect.left)/2  - window.innerWidth/2
+    distance *= -1;
+    if (instant) {
+        galleryList.style.left = Number(galleryList.style.left.slice(0, -2)) + distance +'px';
+        selected.classList.add('done');
+        return;
+    }
+    var speed = 10;
+    var dir = Math.sign(distance) * speed;
+    galleryList.style.left = Number(galleryList.style.left.slice(0, -2)) + dir +'px'
+    if (dir > Math.abs(distance)) {
+        selected.classList.add('done');
+        return;
+    }
+    galleryScrollDelay = setTimeout(goToGalleryImg, 10)
+}
+
+
+goback.addEventListener( 'click', function (e) {
+    if (!galleryList) return;
+    selected = getSelectedGalleryImg();
+    if (!selected) return;
+    selected.classList.remove('selected', 'done');
+    console.log(selected)
+    selected = selected.previousElementSibling || selected;
+    selected.classList.add('selected');
+    goToGalleryImg();
+})
+
+goforward.addEventListener( 'click', function (e) {
+    if (!galleryList) return;
+    selected = getSelectedGalleryImg();
+    if (!selected) return;
+    selected.classList.remove('selected', 'done');
+    console.log(selected)
+    selected = selected.nextElementSibling || selected;
+    selected.classList.add('selected');
+    goToGalleryImg();
+})
+
+
